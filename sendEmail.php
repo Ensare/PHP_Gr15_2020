e135a269b3757c558345928dda9dbad928be9ad8
Alina Fazliu <fazliualina@gmail.com>
2020-05-15 11:14:54 +0200

    Kerkesa e 11

---
+++ sendEmail.php
@@ -0,0 +1,35 @@
+<?php 
+
+function sendEmail($username, $email){
+
+require "phpmailer/PHPMailerAutoload.php";
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 587;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
+
+$mail->Username = "yourhomeofficial00@gmail.com";
+$mail->Password = "phpprojekti1!";
+
+$mail->setFrom("yourhomeofficial00@gmail.com", "Your Home");
+$mail->addAddress($email);
+$mail->addReplyTo("yourhomeofficial00@gmail.com");
+
+$mail->isHTML(true);
+$mail->Subject="Your Home";
+$mail->Body="<h1 align=center>Hello, $username. You have succesfully signed in to our website. To continue there, 
+click the link below <a href=\"http://localhost/PHP_Gr17_2020/index.php\">Your Home</a> </h1>";
+
+if(!$mail -> send()){
+    echo "<script>Alert('Email could not be sent') </script>";}
+else {
+    echo  "Email was sent!";
+}
+
+}
+
+?>
