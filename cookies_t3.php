9c27232ae9e3dae1f43e12fa23491e5a810fe366
AritaAlidemaj <58113020+AritaAlidemaj@users.noreply.github.com>
2020-05-17 21:44:58 +0200

    Update cookies_t3.php

--- cookies_t3.php
+++ cookies_t3.php
@@ -1,58 +1,50 @@
 <?Php
-setcookie ("Plus2netCookie", "", time() - 3600);
-
-
-?>
+   setcookie ("YourHome", "", time() - 3600);
+   
+   
+   ?>
 <!doctype html public "-//w3c//dtd html 3.2//en">
-
 <html>
-
-<head en="lang">
-    <meta charset="utf-8">
-    <title>Your Home</title>
-    <link href='https://fonts.googleapis.com/css?family=David Libre' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Alegreya Sans SC' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Allan' rel='stylesheet'>
-    <link rel="stylesheet" type="text/css" href="lib/reset.css">
-    <link rel="stylesheet" href="lib/animate.css">
-    <link rel="stylesheet" href="lib/stilizimi.css">
-    <script src="javascript/jquery-ui.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="javascript/jquery-3.4.1.min.js"></script>
-<title>(Type a title for your page here)</title>
-</head>
-
-<body>
-<style>
-#paragrafi
-{
-    text-align:center;
-    padding: 50px;
-    font-face:Verdana;
-    size:2;
-}
-p
-{
-    text-align:center;
-    font-family: Alegreya Sans SC; 
-    font-size:25px;
-    padding: 0 2px 0 20px;
-    padding-top: 20px;
-    margin: 20px 59px 20px 20px;
-
-}
-a 
-{
-    font-size: 25px;
-    margin: 200px 20px;
-}
-</style>
-
-<p id=paragrafi>Cookie is removed. You can check this by going to Check the cookie page. Checking the status of the cookie. </p>
-<br><br><br>
-<p><a href=cookies_t1.php>Start a new cookie </a> | <a href=cookies_t2.php>Check the cookie </a> | <a href=cookies_t3.php>Remove the cookie </a>
-</p>
-</body>
-
+   <head en="lang">
+      <meta charset="utf-8">
+      <title>Your Home</title>
+      <link href='https://fonts.googleapis.com/css?family=David Libre' rel='stylesheet'>
+      <link href='https://fonts.googleapis.com/css?family=Alegreya Sans SC' rel='stylesheet'>
+      <link href='https://fonts.googleapis.com/css?family=Allan' rel='stylesheet'>
+      <link rel="stylesheet" type="text/css" href="lib/reset.css">
+      <link rel="stylesheet" href="lib/animate.css">
+      <link rel="stylesheet" href="lib/stilizimi.css">
+      <script src="javascript/jquery-ui.min.js"></script>
+      <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script src="javascript/jquery-3.4.1.min.js"></script>
+   </head>
+   <body>
+      <style>
+         #paragrafi
+         {
+         text-align:center;
+         padding: 50px;
+         font-face:Verdana;
+         size:2;
+         }
+         p
+         {
+         text-align:center;
+         font-family: Alegreya Sans SC; 
+         font-size:25px;
+         padding: 0 2px 0 20px;
+         padding-top: 20px;
+         margin: 20px 59px 20px 20px;
+         }
+         a 
+         {
+         font-size: 25px;
+         margin: 200px 20px;
+         }
+      </style>
+      <p id=paragrafi>Cookie is removed. You can check this by going to Check the cookie page. Checking the status of the cookie. </p>
+      <br><br><br>
+      <p><a href=cookies_t1.php>Start a new cookie </a> | <a href=cookies_t2.php>Check the cookie </a> | <a href=cookies_t3.php>Remove the cookie </a></p>
+   </body>
 </html>
