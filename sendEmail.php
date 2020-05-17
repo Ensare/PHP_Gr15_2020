5e7806dc2e1970fc2138e0a2bd2c04d891b2d67e
Alina Fazliu <fazliualina@gmail.com>
2020-05-17 12:37:47 +0200

    Shtimi i funksioneve

--- sendEmail.php
+++ sendEmail.php
@@ -1,35 +1,64 @@
-<?php 
+<?php
 
-function sendEmail($username, $email){
+function sendEmail($username, $email)
+{
 
-require "phpmailer/PHPMailerAutoload.php";
+    require "phpmailer/PHPMailerAutoload.php";
 
-$mail = new PHPMailer;
-$mail->isSMTP();
+    $mail = new PHPMailer;
+    $mail->isSMTP();
 
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 587;
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'tls';
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
 
-$mail->Username = "yourhomeofficial00@gmail.com";
-$mail->Password = "phpprojekti1!";
+    $mail->Username = "yourhomeofficial00@gmail.com";
+    $mail->Password = "phpprojekti1!";
 
-$mail->setFrom("yourhomeofficial00@gmail.com", "Your Home");
-$mail->addAddress($email);
-$mail->addReplyTo("yourhomeofficial00@gmail.com");
+    $mail->setFrom("yourhomeofficial00@gmail.com", "Your Home");
+    $mail->addAddress($email);
+    $mail->addReplyTo("yourhomeofficial00@gmail.com");
 
-$mail->isHTML(true);
-$mail->Subject="Your Home";
-$mail->Body="<h1 align=center>Hello, $username. You have succesfully signed in to our website. To continue there, 
+    $mail->isHTML(true);
+    $mail->Subject = "Your Home";
+    $mail->Body = "<h1 align=center>Hello, $username. You have succesfully signed in to our website. To continue there, 
 click the link below <a href=\"http://localhost/PHP_Gr17_2020/index.php\">Your Home</a> </h1>";
 
-if(!$mail -> send()){
-    echo "<script>Alert('Email could not be sent') </script>";}
-else {
-    echo  "Email was sent!";
+    if (!$mail->send()) {
+        echo "<script>Alert('Email could not be sent') </script>";
+    } else {
+        echo  "Email was sent!";
+    }
 }
 
-}
+function confirmOrder($email, $username, $total)
+{
+    require "phpmailer/PHPMailerAutoload.php";
 
-?>
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+
+    $mail->Username = "yourhomeofficial00@gmail.com";
+    $mail->Password = "phpprojekti1!";
+
+    $mail->setFrom("yourhomeofficial00@gmail.com", "Your Home");
+    $mail->addAddress($email);
+    $mail->addReplyTo("yourhomeofficial00@gmail.com");
+
+    $mail->isHTML(true);
+    $mail->Subject = "Your Home";
+    $mail->Body = "<h1 align=center>Hello, $username. Your order of $total $ has been succesful. You will be notified further
+in the coming days. Hope you will choose us again, Your Home.</h1>";
+
+    if (!$mail->send()) {
+        echo "<script>Alert('Email could not be sent') </script>";
+    } else {
+        echo "<script>confirm('The status of your order will be emailed to you!')</script>";
+    }
+}
