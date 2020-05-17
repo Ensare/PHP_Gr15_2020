f86791fbdbd93ceba15d03c7a83076245983f6d9
AritaAlidemaj <58113020+AritaAlidemaj@users.noreply.github.com>
2020-05-17 21:41:00 +0200

    Nje ndryshim te cookie

--- cookies_t1.php
+++ cookies_t1.php
@@ -1,64 +1,55 @@
 <!doctype html public "-//w3c//dtd html 3.2//en">
-
 <html>
-
-<head en="lang">
-    <meta charset="utf-8">
-    <title>Your Home</title>
-    <link href='https://fonts.googleapis.com/css?family=David Libre' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Alegreya Sans SC' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Allan' rel='stylesheet'>
-    <link rel="stylesheet" type="text/css" href="lib/reset.css">
-    <link rel="stylesheet" href="lib/animate.css">
-    <link rel="stylesheet" href="lib/stilizimi.css">
-    <script src="javascript/jquery-ui.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="javascript/jquery-3.4.1.min.js"></script>
-<style>
-#forma1
-{
-    margin-top: 45px;
-}
-input[type=text]
- {
-  width: 15%;
-  padding: 7px 13px;
-  margin: 8px 0;
-  box-sizing: border-box;
-
-}
-p
-{
-    text-align:center;
-    font-family: Alegreya Sans SC; 
-    font-size:25px;
-    padding: 0 2px 0 20px;
-    padding-top: 20px;
-    margin: 20px 59px 20px 20px;
-
-}
-a 
-{
-    font-size: 25px;
-    margin: 200px 20px;
-}
-</style>
-
-
-</head>
-
-<body  text="#000000" link="#0000ff" vlink="#800080" alink="#ff0000">
-<form method=post id='forma1' action=cookies_redirect.php  style="text-align: center;  " >
-<p >Enter Your Name
-</p><input type=text name=name  >
-
-<button id="btn1">Next</button>
- 
- </form>
-<br><br><br>
-<p><a href=cookies_t1.php>Start a new cookie </a> | <a href=cookies_t2.php>Check the cookie </a> | <a href=cookies_t3.php>Remove the cookie </a>
-</p>
-</body>
-
+   <head en="lang">
+      <meta charset="utf-8">
+      <title>Your Home</title>
+      <link href='https://fonts.googleapis.com/css?family=David Libre' rel='stylesheet'>
+      <link href='https://fonts.googleapis.com/css?family=Alegreya Sans SC' rel='stylesheet'>
+      <link href='https://fonts.googleapis.com/css?family=Allan' rel='stylesheet'>
+      <link rel="stylesheet" type="text/css" href="lib/reset.css">
+      <link rel="stylesheet" href="lib/animate.css">
+      <link rel="stylesheet" href="lib/stilizimi.css">
+      <script src="javascript/jquery-ui.min.js"></script>
+      <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script src="javascript/jquery-3.4.1.min.js"></script>
+      <style>
+         #forma1
+         {
+         margin-top: 45px;
+         text-align: center;
+         }
+         input[type=text]
+         {
+         width: 15%;
+         padding: 7px 13px;
+         margin: 8px 0;
+         box-sizing: border-box;
+         }
+         p
+         {
+         text-align:center;
+         font-family: Alegreya Sans SC; 
+         font-size:25px;
+         padding: 0 2px 0 20px;
+         padding-top: 20px;
+         margin: 20px 59px 20px 20px;
+         }
+         a 
+         {
+         font-size: 25px;
+         margin: 200px 20px;
+         }
+      </style>
+   </head>
+   <body  >
+      <form method=post id='forma1' action=cookies_redirect.php>
+         <p >Enter Your Name</p>
+         <input type=text name=name>
+         <button id="btn1">Next</button>
+      </form>
+      <br><br><br>
+      <p><a href=cookies_t1.php>Start a new cookie </a> | <a href=cookies_t2.php>Check the cookie </a> | <a href=cookies_t3.php>Remove the cookie </a></p>
+   </body>
 </html>
+COPY TO CLIPBOARD	
